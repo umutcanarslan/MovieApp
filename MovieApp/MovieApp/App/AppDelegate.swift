47428888f9d92702ef99cb489f371dcb9b2dcb2b
Umut Can Arslan <umutcan.arslan1998@hotmail.com>
2022-10-31 21:48:17 +0300

    Create app with initial configuration

---
+++ MovieApp/MovieApp/App/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  MovieApp
+//
+//  Created by Umut Can Arslan on 31.10.2022.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        window = UIWindow(frame: UIScreen.main.bounds)
+
+        let viewController: UIViewController = .init()
+        viewController.view.backgroundColor = .red
+        window?.rootViewController = viewController
+        window?.makeKeyAndVisible()
+
+        return true
+    }
+
+}
