ecccf622badfb9e31a63986f9852e56f4c7fbdaa
Umut Can Arslan <umutcan.arslan1998@hotmail.com>
2022-11-15 16:58:59 +0300

    Create movie list cell files

---
+++ MovieApp/MovieApp/CustomViews/MovieListCell/MovieListTableViewCell.swift
@@ -0,0 +1,21 @@
+//
+//  MovieListTableViewCell.swift
+//  MovieApp
+//
+//  Created by Umut Can Arslan on 15.11.2022.
+//
+
+import UIKit
+
+final class MovieListTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var movieAverage: UILabel!
+    @IBOutlet weak var movieTitle: UILabel!
+    @IBOutlet weak var movieOverview: UILabel!
+    @IBOutlet weak var movieImage: UIImageView!
+
+    func configureMovieCell() {
+        // TODO: add configure code for movie cell information
+    }
+    
+}
